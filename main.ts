c21c03bc2f4f4325baaef56f9a34c47f3124d9f1
McTom234 <mc_tom234@gmx.de>
2023-12-14 12:19:07 +0100

    feat: initial commit

---
+++ main.ts
@@ -0,0 +1,61 @@
+import * as process from "process";
+import * as fs from "fs";
+import * as path from "path";
+import { Day } from "./types";
+
+const inFile = process.argv[2];
+const nameFilter = process.argv[3];
+const outFile = process.argv[4];
+
+const nameFilters = nameFilter?.split(",");
+
+const inputFileContents = JSON.parse(
+  fs.readFileSync(path.join(__dirname, inFile ?? "in.json"), "utf8"),
+) as Day[];
+
+const days: Day[] = [];
+
+for (const inputFileContent of inputFileContents) {
+  days.push({
+    date: inputFileContent.date,
+    projects: inputFileContent.projects.map((project) => ({
+      name: project.name,
+      branches: project.branches.map(
+        ({ name, minutes, seconds, total_seconds, hours }) => ({
+          name,
+          total_seconds,
+          hours,
+          seconds,
+          minutes,
+        }),
+      ),
+    })),
+  });
+}
+
+let filteredDays: Day[];
+
+for (const day of days) {
+  filteredDays = days
+    .map((day) => {
+      day.projects = day.projects
+        .map((project) => {
+          project.branches = project.branches.filter(
+            (branch) => branch.total_seconds > 0,
+          );
+          return project;
+        })
+        .filter(
+          (project) =>
+            project.branches.length > 0 &&
+            nameFilters?.includes(project.name)
+        );
+      return day;
+    })
+    .filter((day) => day.projects.length > 0);
+}
+
+fs.writeFileSync(
+  path.join(__dirname, outFile ?? "out.json"),
+  JSON.stringify(filteredDays, null, 2),
+);
